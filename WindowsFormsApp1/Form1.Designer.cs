334824c97ad79c1b28845b8fa9e04ee30de90c27
yanwei chen <1843008835@qq.com>
2019-07-14 18:48:17 +0900

    commit

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.EXIT_Button = new System.Windows.Forms.Button();
+            this.LOGIN_Button = new System.Windows.Forms.Button();
+            this.USERNAME_textBox1 = new System.Windows.Forms.TextBox();
+            this.PASSWORD_textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // EXIT_Button
+            // 
+            this.EXIT_Button.Location = new System.Drawing.Point(552, 330);
+            this.EXIT_Button.Name = "EXIT_Button";
+            this.EXIT_Button.Size = new System.Drawing.Size(130, 60);
+            this.EXIT_Button.TabIndex = 0;
+            this.EXIT_Button.Text = "EXIT";
+            this.EXIT_Button.UseVisualStyleBackColor = true;
+            this.EXIT_Button.Click += new System.EventHandler(this.EXIT_Button_Click);
+            // 
+            // LOGIN_Button
+            // 
+            this.LOGIN_Button.Location = new System.Drawing.Point(787, 330);
+            this.LOGIN_Button.Name = "LOGIN_Button";
+            this.LOGIN_Button.Size = new System.Drawing.Size(130, 60);
+            this.LOGIN_Button.TabIndex = 1;
+            this.LOGIN_Button.Text = "LOGIN";
+            this.LOGIN_Button.UseVisualStyleBackColor = true;
+            this.LOGIN_Button.Click += new System.EventHandler(this.LOGIN_Button_Click);
+            // 
+            // USERNAME_textBox1
+            // 
+            this.USERNAME_textBox1.Location = new System.Drawing.Point(552, 99);
+            this.USERNAME_textBox1.Name = "USERNAME_textBox1";
+            this.USERNAME_textBox1.Size = new System.Drawing.Size(365, 31);
+            this.USERNAME_textBox1.TabIndex = 2;
+            // 
+            // PASSWORD_textBox2
+            // 
+            this.PASSWORD_textBox2.Location = new System.Drawing.Point(552, 197);
+            this.PASSWORD_textBox2.Name = "PASSWORD_textBox2";
+            this.PASSWORD_textBox2.Size = new System.Drawing.Size(365, 31);
+            this.PASSWORD_textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(406, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "USERNAME";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(406, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "PASSWORD";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1044, 516);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PASSWORD_textBox2);
+            this.Controls.Add(this.USERNAME_textBox1);
+            this.Controls.Add(this.LOGIN_Button);
+            this.Controls.Add(this.EXIT_Button);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button EXIT_Button;
+        private System.Windows.Forms.Button LOGIN_Button;
+        private System.Windows.Forms.TextBox USERNAME_textBox1;
+        private System.Windows.Forms.TextBox PASSWORD_textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+
 
 namespace WindowsFormsApp1
 {
@@ -15,6 +17,32 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void EXIT_Button_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void LOGIN_Button_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("server =DESKTOP-DBENKHM;database=mydatabase1;uid=sa;pwd=dhc3b@dhc");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select count(*) from login where username='" + USERNAME_textBox1.Text + "' and password='" + PASSWORD_textBox2.Text + "'",con);
+            DataTable da = new DataTable();
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            sda.Fill(da);
+            if (da.Rows[0][0].ToString() == "1") 
+            {
+                this.Hide();
+                Form2 form2 = new Form2();
+                form2.Show();
+            }
+            else
+            {
+                MessageBox.Show("the username or password is wrong");
+            }
+
         }
     }
 }
